28c971399ebfb41190ff146798bae71dcad67d93
KellyReedKC <>
2021-06-08 21:33:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let language = 'JavaScript';
+
+//a) Use string concatenation and two .slice() methods to print 'JS' from 'JavaScript'.
+
+console.log(language.slice(0,1) + language.slice(4,5));
+let initials = (language.slice(0,1) + language.slice(4,5));
+console.log(initials);
+
+//b) Without using .slice(), use method chaining to accomplish the same thing.
+
+
+
+//c) Use bracket notation and a template literal to print "The abbreviation for 'JavaScript' is 'JS'."
+
+console.log(`The abbreviation for ${language} is ${language.slice(0,1) + language.slice(4,5)}`)
+
+//d) Just for fun, try chaining 3 or more methods together, and then print the result.
